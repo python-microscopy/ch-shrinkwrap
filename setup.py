279fb3b384e030494955f186f9c4a2184fc4dbd6
zacsimile <zachcm@gmail.com>
2022-06-04 13:18:57 -0500

    Update setup to run install_pyme_plugin()

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ from Cython.Build import cythonize
 
 this_dir = os.path.dirname(__file__)
 
+def install_pyme_plugin():
+    import sys
+    import subprocess
+    import os
+    plugin_install_path = os.path.join(this_dir,
+                                       'install_plugin.py')
+    subprocess.Popen('%s %s' % (sys.executable, plugin_install_path), 
+                        shell=True)
+
 module0 = Extension('ch_shrinkwrap.membrane_mesh_utils', sources=[os.path.join(this_dir, 'ch_shrinkwrap/membrane_mesh_utils.c')])
 module1 = Extension('ch_shrinkwrap._membrane_mesh', sources=[os.path.join(this_dir, 'ch_shrinkwrap/_membrane_mesh.pyx')], include_dirs=get_numpy_include_dirs())
 # module2 = Extension('ch_shrinkwrap._conj_grad', sources=[os.path.join(this_dir, 'ch_shrinkwrap/_conj_grad.pyx')], include_dirs=get_numpy_include_dirs())
@@ -23,3 +32,5 @@ setup(name='ch_shrinkwrap',
       packages=find_packages(),
       ext_modules = cythonize([module0, module1, module3])
      )
+
+install_pyme_plugin()
