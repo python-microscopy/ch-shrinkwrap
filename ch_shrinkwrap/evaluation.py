236cdb439b809b70f20cd536b0512f0757b79874
zacsimile <zachcm@gmail.com>
2022-08-30 20:09:09 -0500

    More variables

--- ch_shrinkwrap/evaluation.py
+++ ch_shrinkwrap/evaluation.py
@@ -1,3 +1,4 @@
+from cgi import test
 import os
 import uuid
 
@@ -12,21 +13,30 @@ def generate_pointclouds(test_d, output_dir, inputs=None):
     recipe_text = f"""
     - simulation.PointcloudFromShape:
         output: test
+        shape_name: {test_d['shape_name']}
+        shape_params: {test_d['shape_params']}
+        density: {test_d['density']}
         psf_width_x: {test_d['psf_width'][0]}
         psf_width_y: {test_d['psf_width'][1]}
         psf_width_z: {test_d['psf_width'][2]}
+        mean_photon_count: {test_d['mean_photon_count']}
+        bg_photon_count: {test_d['bg_photon_count']}
+        noise_fraction: {test_d['noise_fraction']}
+        p: {test_d['p']}
     - output.HDFOutput:
-        filePattern: '{{output_dir}}/{test_pointcloud_id}.hdf'
+        filePattern: '{{output_dir}}/test_{test_pointcloud_id}.hdf'
         inputVariables:
             test: test
         scheme: pyme-cluster://
     - simulation.PointcloudFromShape:
+        shape_name: {test_d['shape_name']}
+        shape_params: {test_d['shape_params']}
         density: {test_d['density']}
         no_jitter: true
         output: shape
         p: {test_d['p']}
     - output.HDFOutput:
-        filePattern: '{{output_dir}}/{shape_pointcloud_id}.hdf'
+        filePattern: '{{output_dir}}/shape_{shape_pointcloud_id}.hdf'
         inputVariables:
             shape: shape
         scheme: pyme-cluster://
@@ -37,6 +47,42 @@ def generate_pointclouds(test_d, output_dir, inputs=None):
     rule.push()
 
     return test_pointcloud_id, shape_pointcloud_id
+
+def compute_shrinkwrap(test_d, output_dir, inputs=None):
+    recipe_text = f"""
+    - pointcloud.Octree:
+        input_localizations: two_toruses
+        output_octree: octree
+    - surface_fitting.DualMarchingCubes:
+        input: octree
+        output: mesh
+        remesh: true
+        threshold_density: {test_d['threshold_density']}
+        n_points_min: {test_d['n_points_min']}
+    - surface_fitting.ShrinkwrapMembrane:
+        input: mesh
+        max_iters: {test_d['max_iter']}
+        curvature_weight: {test_d['curvature_weight']}      
+        remesh_frequency: {test_d['remesh_frequency']}
+        punch_frequency: {test_d['punch_frequency']}
+        min_hole_radius: {test_d['min_hole_radius']}
+        neck_theshold_low: {test_d['neck_theshold_low']}
+        neck_threshold_high: {test_d['neck_threshold_high']}
+        neck_first_iter: {test_d['neck_first_iter']}
+        output: membrane
+        points: two_toruses
+    - surface_feature_extraction.PointsFromMesh:
+        input: membrane
+        output: membrane0_localizations
+    - surface_feature_extraction.AverageSquaredDistance:
+        input: membrane0_localizations
+        input2: two_toruses_raw
+        output: average_squared_distance
+    """
+
+    rule = RecipeRule(recipe=recipe_text, output_dir=output_dir, inputs=inputs)
+
+    rule.push()
 
 def evaluate(file_name):
     with open(file_name) as f:
