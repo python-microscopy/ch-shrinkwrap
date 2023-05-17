4c581b07a1a566acdee45c49af060360a1d25692
David Baddeley <david.baddeley.nz@gmail.com>
2023-05-17 14:53:15 +1200

    improved residual plot

--- ch_shrinkwrap/util.py
+++ ch_shrinkwrap/util.py
@@ -54,25 +54,65 @@ def surf_residuals(surf, points, sigma):
     d = isosurface.distance_to_mesh(points, surf, smooth=False)
 
     f = plt.figure()
-    a1, a2 = f.subplots(2, 1)
-    a1.hist(d, np.linspace(-100, 100, 500))
+    a1 = f.subplots(1, 1)
+    a1.hist(d, np.linspace(-100, 100, 500), density=True)
     a1.grid()
     a1.set_xlabel('Distance from surface [nm]')
     a1.set_ylabel('Frequency')
     a1.set_title('Surface residuals')
 
-    #a = plt.subplot()
-    a2.hist(np.abs(d), np.linspace(0, 100, 500), density=True, label='Experimental')
+    ##a = plt.subplot()
+    #a2.hist(np.abs(d), np.linspace(0, 100, 500), density=True, label='Experimental')
 
     me = np.median(sigma)
-    print(me)
-    x = np.linspace(0, 100, 1000)
-    a2.plot(x, stats.chi(3).pdf(x/me)/(me), label='Predicted')
+    #print(me)
+    x = np.linspace(-100, 100, 1000)
+    a1.plot(x, 0.5*stats.chi(3).pdf(np.abs(x)/me)/(me), label='Predicted')
 
-    a2.grid()
+    #a2.grid()
     #a2.set_xlabel('Squared distance [nm^2]')
-    a2.set_xlabel('Absolute distance from surface [nm]')
-    a2.set_ylabel('Frequency')
-    a2.legend()
+    #a2.set_xlabel('Absolute distance from surface [nm]')
+    #a2.set_ylabel('Frequency')
+    a1.legend()
+
+    # f = plt.figure()
+    # a1, a2 = f.subplots(2, 1)
+    # c, _, _ = a2.hist(np.abs(d), np.linspace(0, 100, 500), density=True, label='Experimental')
+    # print('c.sum()', c.sum())
+    
+    # b = np.arange(0, 30, 1)
+    # c, _ = np.histogram(sigma.ravel(), b, density=True)
+    # bm = 0.5*(b[:-1] + b[1:])
+
+    # d_ = 0
+    # #print(b, c, bm, sigma)
+    # for c_, b_ in zip(c, bm):
+    #     for r in np.arange(0.09, 1.01, .1):
+    #         p = 0.1*(3/2)*(1.0-r*r)
+    #         d_ += p*c_*stats.chi(3).pdf(x/(b_*r))/(b_*r)
+
+    # print('d_.sum()', d_.sum())
+    # a2.plot(x, 0.5*d_, label='Predicted')
+
+    # # d_ = 0
+    # # #print(b, c, bm, sigma)
+    # # #for c_, b_ in zip(c, bm):
+    # # for r in np.arange(0.09, 1.01, .1):
+    # #     p = 0.1*(3/2)*(1.0-r*r)
+    # #     d_ += p*stats.chi(3).pdf(x/(me*r))/(me*r)
+
+    # # print('d_.sum()', d_.sum())
+    # # a2.plot(x, 0.5*d_, label='Predicted')
+
+    # a2.grid()
+    # #a2.set_xlabel('Squared distance [nm^2]')
+    # a2.set_xlabel('Absolute distance from surface [nm]')
+    # a2.set_ylabel('Frequency')
+    # a2.legend()
+
+    
+
+
+
 
 
