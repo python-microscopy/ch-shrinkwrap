42c0857ee702612f9db2e22c48aee543a67adfef
David Baddeley <david.baddeley.nz@gmail.com>
2026-08-27 11:45:50 +1200

    drop numpy distutils and add pyproject.toml and a github workflow for building pip wheels

--- install_plugin.py
+++ install_plugin.py
@@ -1,16 +1,16 @@
 from PYME import config
 import os
 import sys
-from distutils.dir_util import copy_tree
+import shutil
 
 def main():
-    this_dir = os.path.dirname(__file__)
+    this_dir = os.path.dirname(os.path.abspath(__file__))
 
     try:
         if sys.argv[1] == 'dist':
-            copy_tree(os.path.join(this_dir, '_etc', 'PYME'), config.dist_config_directory)
+            shutil.copytree(os.path.join(this_dir, '_etc', 'PYME'), config.dist_config_directory, dirs_exist_ok=True)
     except IndexError:  # no argument provided, default to user config directory
-        copy_tree(os.path.join(this_dir, '_etc', 'PYME'), config.user_config_dir)
+        shutil.copytree(os.path.join(this_dir, '_etc', 'PYME'), config.user_config_dir, dirs_exist_ok=True)
 
 if __name__ == '__main__':
     main()
